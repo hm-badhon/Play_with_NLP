3103dc63a390ec78d1ac3293d7215246fd5051c0
hm-badhon <h.m.badhoneee@gmail.com>
2025-02-17 17:24:38 +0600

    updated file....

--- 5_Chunking_Technique/1.Fixed_size_chunk/fixed_size_chunking_code.py
+++ 5_Chunking_Technique/1.Fixed_size_chunk/fixed_size_chunking_code.py
@@ -4,6 +4,8 @@ def fixed_size_chuning(text, chunk_size, overlap):
     chunks = []
 
     for i in range(0, len(words), chunk_size - overlap):
+        print('chunk_size',chunk_size)
+        print('overlap', overlap)
         chunk = words[i:i + chunk_size]
         print(chunk)
         chunks.append(" ".join(chunk))
@@ -20,4 +22,3 @@ chunks = fixed_size_chuning(text, 10,3)
 
 for i , chunk in enumerate(chunks):
     print(f"Chunk {i+1}: {chunk}\n")
-    
